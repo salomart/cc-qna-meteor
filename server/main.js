7778105f8d8b4f5f90ee5723627fb53cba5bf17e
Salomon Martinez <salomart@outlook.com>
2019-02-23 23:53:37 -0600

    Added Assignment 3

--- server/main.js
+++ server/main.js
@@ -36,5 +36,48 @@ Meteor.methods({
 		
 		var data = fut.wait();
 		return data ? data : [];
+	},
+	'getTimeOne': function(tuples, rowLimit, numOfQueries) {
+		var startTime = new Date();
+		
+		for (i=0; i<numOfQueries; i++) {
+			var fut = new Future();
+			
+			connection.query('SELECT ' + tuples + ' FROM q2quakes ORDER BY RAND() LIMIT ' + rowLimit,
+					function (error, results, fields) {
+				if (!error) {
+					fut.return(results);
+				} else {
+					console.log(error);
+				}
+			});
+			
+			var data = fut.wait();
+		}
+		
+		var endTime = new Date();
+		return endTime - startTime;
+	},
+	'getTimeTwo': function(tuples, rowLimit, numOfQueries, net) {
+		var queryStr = 'SELECT ' + tuples
+			+ ' FROM q2quakes WHERE net = ? ORDER BY RAND() LIMIT ' + rowLimit;
+		var startTime = new Date();
+		
+		for (i=0; i<numOfQueries; i++) {
+			var fut = new Future();
+			
+			connection.query(queryStr, net, function (error, results, fields) {
+				if (!error) {
+					fut.return(results);
+				} else {
+					console.log(error);
+				}
+			});
+			
+			var data = fut.wait();
+		}
+		
+		var endTime = new Date();
+		return endTime - startTime;
 	}
 });
