2d38458f073e458325b47bd204f74ecb728e0ee6
Salomon Martinez <salomart@outlook.com>
2019-03-05 17:40:08 -0600

    Updated Quiz 3

--- client/pages/quizzes/quiz3.js
+++ client/pages/quizzes/quiz3.js
@@ -1,63 +1,47 @@
-var q3TaskOneTime = new ReactiveVar(null);
-var q3TaskTwoTime = new ReactiveVar(null);
-var q3TaskThreeTime = new ReactiveVar(null);
-var q3TaskFourTime = new ReactiveVar(null);
-var q3TaskSeven = new ReactiveVar(null);
-var q3TaskEight = new ReactiveVar([]);
-var q3TaskNine = new ReactiveVar([]);
+var population = new ReactiveVar(null);
+var counties = new ReactiveVar([]);
+var states = new ReactiveVar([]);
 
 Template.quiz3.helpers({
-	'taskOneTime': function() {
-		return q3TaskOneTime.get();
-	},
-	'taskTwoTime': function() {
-		return q3TaskTwoTime.get();
-	},
-	'taskThreeTime': function() {
-		return q3TaskThreeTime.get();
-	},
-	'taskFourTime': function() {
-		return q3TaskFourTime.get();
-	},
 	'population': function() {
-		return q3TaskSeven.get();
+		return population.get();
 	},
 	'countyCount': function() {
-		return q3TaskEight.get().length;
+		return counties.get().length;
 	},
 	'counties': function() {
-		return q3TaskEight.get();
+		return counties.get();
 	},
 	'states': function() {
-		return q3TaskNine.get();
+		return states.get();
 	}
 });
 
 Template.quiz3.events({
-	'submit .q3task7': function(event) {
+	'submit .getPopByStateYear': function(event) {
 		event.preventDefault();
 		
 		var state = event.target.state.value;
 		var year = event.target.year.value;
 		
-		Meteor.call('getQ3TimeSeven', state, year, function(error, result) {
+		Meteor.call('getPopByStateYear', state, year, function(error, result) {
 			if (!error) {
-				q3TaskSeven.set(result);
+				population.set(result);
 			}
 		});
 	},
-	'submit .q3task8': function(event) {
+	'submit .getCountiesByState': function(event) {
 		event.preventDefault();
 		
 		var state = event.target.state.value;
 		
-		Meteor.call('getQ3TimeEight', state, function(error, result) {
+		Meteor.call('getCountiesByState', state, function(error, result) {
 			if (!error) {
-				q3TaskEight.set(result);
+				counties.set(result);
 			}
 		});
 	},
-	'submit .q3task9': function(event) {
+	'submit .getStatesByPopRange': function(event) {
 		event.preventDefault();
 		
 		var year = event.target.year.value;
@@ -65,83 +49,11 @@ Template.quiz3.events({
 		var maxPop = event.target.maxPop.value;
 		
 		if (year >= 2010 && year <= 2018) {
-			Meteor.call('getQ3TimeNine', year, minPop, maxPop, function(error, result) {
+			Meteor.call('getStatesByPopRange', year, minPop, maxPop, function(error, result) {
 				if (!error) {
-					q3TaskNine.set(result);
+					states.set(result);
 				}
 			});
 		}
-	}/*,
-	'submit .q3task1': function(event) {
-		event.preventDefault();
-		
-		var tuples = event.target.tuples.value;
-		tuples = tuples ? tuples : "1";
-		var numOfQueries = event.target.numOfQueries.value;
-		numOfQueries ? numOfQueries : "1";
-		
-		Meteor.call('getQ3TimeOne', tuples, numOfQueries, function(error, result) {
-			if (!error) {
-				q3TaskOneTime.set(result);
-			}
-		});
-	},
-	'submit .q3task2': function(event) {
-		event.preventDefault();
-		
-		var tuples = event.target.tuples.value;
-		tuples = tuples ? tuples : "1";
-		var numOfQueries = event.target.numOfQueries.value;
-		numOfQueries ? numOfQueries : "1";
-		var year = event.target.year.value;
-		year = year ? year : "0";
-		var minPop = event.target.minPop.value;
-		minPop = minPop ? minPop : "0";
-		var maxPop = event.target.maxPop.value;
-		maxPop = maxPop ? maxPop : "50000000";
-		
-		if (year >= 2010 && year <= 2018) {
-			Meteor.call('getQ3TimeTwo', tuples, numOfQueries, year, minPop, maxPop, function(error, result) {
-				if (!error) {
-					q3TaskTwoTime.set(result);
-				}
-			});
-		}
-	},
-	'submit .q3task3': function(event) {
-		event.preventDefault();
-		
-		var tuples = event.target.tuples.value;
-		tuples = tuples ? tuples : "1";
-		var numOfQueries = event.target.numOfQueries.value;
-		numOfQueries ? numOfQueries : "1";
-		
-		Meteor.call('getQ3TimeThree', tuples, numOfQueries, function(error, result) {
-			if (!error) {
-				q3TaskThreeTime.set(result);
-			}
-		});
-	},
-	'submit .q3task4': function(event) {
-		event.preventDefault();
-		
-		var tuples = event.target.tuples.value;
-		tuples = tuples ? tuples : "1";
-		var numOfQueries = event.target.numOfQueries.value;
-		numOfQueries ? numOfQueries : "1";
-		var year = event.target.year.value;
-		year = year ? year : "0";
-		var minPop = event.target.minPop.value;
-		minPop = minPop ? minPop : "0";
-		var maxPop = event.target.maxPop.value;
-		maxPop = maxPop ? maxPop : "50000000";
-		
-		if (year >= 2010 && year <= 2018) {
-			Meteor.call('getQ3TimeFour', tuples, numOfQueries, year, minPop, maxPop, function(error, result) {
-				if (!error) {
-					q3TaskTwoTime.set(result);
-				}
-			});
-		}
-	}*/
+	}
 });
--- server/main.js
+++ server/main.js
@@ -153,111 +153,7 @@ Meteor.methods({
 		var endTime = new Date();
 		return endTime - startTime;
 	},
-	'getQ3TimeOne': function(tuples, numOfQueries) {
-		var startTime = new Date();
-		
-		for (i=0; i<numOfQueries; i++) {
-			var fut = new Future();
-			
-			connection.query('SELECT * FROM q3counties ORDER BY RAND() LIMIT ' + tuples,
-					function (error, results, fields) {
-				if (!error) {
-					fut.return(results);
-				} else {
-					console.log(error);
-					fut.return([]);
-				}
-			});
-			
-			var data = fut.wait();
-		}
-		
-		var endTime = new Date();
-		return endTime - startTime;
-	},
-	'getQ3TimeTwo': function(tuples, numOfQueries, year, minPop, maxPop) {
-		var queryStr = 'SELECT County FROM q3counties INNER JOIN q3population '
-			+ 'ON q3counties.State = q3population.State WHERE ? >= ? AND ? <= ? '
-			+ 'ORDER BY RAND() LIMIT ' + tuples;
-		var startTime = new Date();
-		
-		for (i=0; i<numOfQueries; i++) {
-			var fut = new Future();
-			
-			connection.query(queryStr, [year, minPop, year, maxPop], function (error, results, fields) {
-				if (!error) {
-					fut.return(results);
-				} else {
-					console.log(error);
-					fut.return([]);
-				}
-			});
-			
-			var data = fut.wait();
-		}
-		
-		var endTime = new Date();
-		return endTime - startTime;
-	},
-	'getQ3TimeThree': function(tuples, numOfQueries) {
-		var cursor = Math.floor(Date.now()/1000);
-		var match = '*Cou*';
-		var startTime = new Date();
-		
-		for (i=0; i<numOfQueries; i++) {
-			var fut = new Future();
-			
-			redisClient.scan(cursor.toString(), 'MATCH', match,
-					'COUNT', tuples).then(function (result) {
-				var pipeline = redisClient.pipeline();
-				
-				for (j=0; j<result[1].length; j++) {
-					pipeline.hgetall(result[1][j]);
-				}
-				
-				pipeline.exec(function (err, results) {
-					fut.return('done');
-				});
-			});
-			
-			cursor = cursor + 1;
-			var data = fut.wait();
-		}
-		
-		var endTime = new Date();
-		return endTime - startTime;
-	},
-	'getQ3TimeFour': function(tuples, numOfQueries, year, minPop, maxPop) {
-		var cursor = Math.floor(Date.now()/1000);
-		var match = '*Pop*';
-		var startTime = new Date();
-		
-		for (i=0; i<numOfQueries; i++) {
-			var fut = new Future();
-			
-			redisClient.keys(match).then(function (result) {
-				var pipeline = redisClient.pipeline();
-				
-				console.log(result);
-				
-				for (j=0; j<result.length; j++) {
-					pipeline.hget(result[j], year);
-				}
-				
-				pipeline.exec(function (err, results) {
-					console.log(results);
-					fut.return('done');
-				});
-			});
-			
-			cursor = cursor + 1;
-			var data = fut.wait();
-		}
-		
-		var endTime = new Date();
-		return endTime - startTime;
-	},
-	'getQ3TimeSeven': function(state, year) {
+	'getPopByStateYear': function(state, year) {
 		var queryStr = 'SELECT `' + year + '` FROM q3population INNER JOIN q3statecode '
 			+ 'ON q3population.State = q3statecode.State WHERE `State Code` = ?';
 		
@@ -265,7 +161,6 @@ Meteor.methods({
 		
 		connection.query(queryStr, state, function (error, results, fields) {
 			if (!error) {
-				console.log(results);
 				fut.return(results);
 			} else {
 				console.log(error);
@@ -276,7 +171,7 @@ Meteor.methods({
 		var data = fut.wait();
 		return data[0][year];
 	},
-	'getQ3TimeEight': function(state, year) {
+	'getCountiesByState': function(state, year) {
 		var queryStr = 'SELECT County FROM q3counties INNER JOIN q3statecode '
 			+ 'ON q3counties.State = q3statecode.State WHERE `State Code` = ?';
 		
@@ -284,7 +179,6 @@ Meteor.methods({
 		
 		connection.query(queryStr, state, function (error, results, fields) {
 			if (!error) {
-				console.log(results);
 				fut.return(results);
 			} else {
 				console.log(error);
@@ -295,14 +189,13 @@ Meteor.methods({
 		var data = fut.wait();
 		return data;
 	},
-	'getQ3TimeNine': function(year, minPop, maxPop) {
+	'getStatesByPopRange': function(year, minPop, maxPop) {
 		var queryStr = 'SELECT State FROM q3population WHERE `' + year + '` >= ? AND `' + year + '` <= ?';
-
+		
 		var fut = new Future();
 		
 		connection.query(queryStr, [minPop, maxPop], function (error, results, fields) {
 			if (!error) {
-				console.log(results);
 				fut.return(results);
 			} else {
 				console.log(error);
