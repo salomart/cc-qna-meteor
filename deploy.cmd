21de2104d898d254e4d6a87f364935de0fc0d0fa
Salomon Martinez <salomart@outlook.com>
2019-03-23 19:21:44 -0500

    Removed deploy script

--- deploy.cmd
+++
@@ -1,6 +0,0 @@
-set x = meteor npm install
-set x = meteor build meteor-build --architecture os.linux.x86_64 --directory
-copy aws-package.json meteor-build\bundle\package.json
-cd meteor-build\bundle\programs\server
-set x = meteor npm install
-set x = meteor npm install fibers@3.1.1
